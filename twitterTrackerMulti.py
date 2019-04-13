0d03995cf3ef1e2f467125973e9fe0ddd1caa0af
JLap <jlap4567@gmail.com>
2019-04-13 07:26:39 -0400

    worked on twitter and make a file with test data

--- twitterTrackerMulti.py
+++ twitterTrackerMulti.py
@@ -11,20 +11,31 @@ access_secret = "XQgV0w0WJ5nfBVkib50OPPbTlBV6IQBJmqpXPaiEdlP16"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
+file = open('today.txt', 'a')
 
+#These are global variables that tract the current users
+users = {}
+threads = []
+
+def trackMood(userName):
+    """
+    This function creates a stream to track a users current mood and handles the accordingly
+    """
+    return 0
+
+def getOverAllMood(userName):
+    """
+    This function takes a string userName and gets the overall mood of a new 
+    user based off of all of their last 200 posts as a double
+    """
+    return 0;
 
 def handleTweet(userName):
     """
-    This functions handles the tweets after it has
-    been picked up by the listener
+    This functions handles the tweets after
     """
-    print(userName)
-    tweets = []
-
-    #gets a list of the last 50 tweets posted by a user
-    for tweet in api.user_timeline(screen_name = userName, count = 50, include_rts = False, tweet_mode = 'extended'):
-        tweets.append(tweet)
-    print(dir(tweets[0]))
+    users[userName] = getOverAllMood()
+    trackMood(userName)
     
 
 class CustomStreamListener(tweepy.StreamListener):
@@ -33,9 +44,8 @@ class CustomStreamListener(tweepy.StreamListener):
 
     def on_data(self, data):
         json_data = json.loads(data)
-        t1 = threading.Thread(target=handleTweet, args=(json_data['user']['screen_name'],)) 
-        t1.start()
-        t1.join()
+        threads.append(threading.Thread(target=handleTweet, args=(json_data['user']['name'],))) 
+        threads[-1].start()
 
     def on_error(self, status_code):
         print(sys.stderr, 'Encountered error with status code:', str(status_code))

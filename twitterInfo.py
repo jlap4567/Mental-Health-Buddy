fcad6130e5eea2a21b1961cd067065b7f82cce59
JLap <jlap4567@gmail.com>
2019-04-13 07:28:34 -0400

    worked on twitter and make a file with test data

--- twitterInfo.py
+++ twitterInfo.py
@@ -3,10 +3,10 @@ import tweepy
 import json
 import threading 
 
-consumer_key = "ehOAPbUM9ip4IVq2Iq8rLmy5e"
-consumer_secret = "00MnazBUO5OY4Prxw0N3Yq88UiEP5QKuuqmAJqv3gN771hKstr"
-access_key = "1061740751288262658-G3vwuy0iwzmS9IGdwiDi29jHah1Vfd"
-access_secret = "XQgV0w0WJ5nfBVkib50OPPbTlBV6IQBJmqpXPaiEdlP16"
+consumer_key = "key"
+consumer_secret = "key"
+access_key = "Key"
+access_secret = "Key"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
